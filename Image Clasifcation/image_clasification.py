ef3f5c9e09dd6bc8dadf782f422d1f38d739fbd6
Szymon Szczurowski <szczurowski.szymon@wp.pl>
2023-12-20 22:06:08 +0100

    Carrying out training and creation of a graph

--- Image Clasifcation/image_clasification.py
+++ Image Clasifcation/image_clasification.py
@@ -8,6 +8,8 @@ import torch.nn.functional as F
 from torch import Tensor
 import torch
 from torch import optim
+from collections import Counter
+import matplotlib.pyplot as plt
 
 mnist = fetch_openml("mnist_784", parser='auto')
 
@@ -16,7 +18,7 @@ targets = np.vectorize(lambda x: int(x))(mnist.target.to_numpy()) # changing a s
 
 print(data.shape)
 print(targets.shape)
-print(targets)
+print(len(set(targets)))
 
 # Data split
 train_data, test_data, train_targets, test_targets = train_test_split(data, targets, train_size=0.8, stratify=targets)
@@ -34,11 +36,12 @@ class DigitsDataset(Dataset):
         x = self.data[idx] / 255
         y = self.targets[idx]
 
-        return x, y
+        return x, torch.tensor(y, dtype=torch.long)
 
 
 train_dataset = DigitsDataset(train_data, train_targets)
 test_dataset = DigitsDataset(test_data, test_targets)
+
 
 # print(train_dataset[0][0])
 # print(test_dataset[0][1])
@@ -66,3 +69,50 @@ class Model(nn.Module):
         if not self.training:
             out = F.softmax(out, dim=1)
         return out
+
+
+# Training
+model = Model(784, 10)
+loss_function = nn.CrossEntropyLoss()
+optimizer = optim.Adam(model.parameters())
+
+train_losses = []
+test_losses = []
+
+for epoch in range(10):
+    epoch_train_loss = 0.0
+    epoch_test_loss = 0.0
+
+    for data in train_dataloader:
+        inputs, labels = data
+
+        optimizer.zero_grad()
+        inputs = inputs.float()
+        outputs = model(inputs)
+        loss = loss_function(outputs, labels)
+        loss.backward()
+        optimizer.step()
+
+        epoch_train_loss += loss.item()
+
+    with torch.no_grad():
+        for test_data in test_dataloader:
+            inputs, labels = test_data
+
+            inputs = inputs.float()
+            outputs = model(inputs)
+            loss = loss_function(outputs, labels)
+
+            epoch_test_loss += loss.item()
+
+    epoch_train_loss /= len(train_dataloader)
+    epoch_test_loss /= len(test_dataloader)
+
+    train_losses.append(epoch_train_loss)
+    test_losses.append(epoch_test_loss)
+
+    print(f"Epoch {epoch} - Train Loss: {epoch_train_loss:.4f}, Test Loss: {epoch_test_loss:.4f}")
+
+
+plt.plot(list(range(40)),train_losses)
+plt.plot(list(range(40)),test_losses)
